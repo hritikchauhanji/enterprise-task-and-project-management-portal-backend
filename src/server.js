b96fb85b64363ea33e207bd32f8b774d8c8e8d75
Hritik Chauhan <hritikchji@gmail.com>
2025-10-23 16:15:01 +0530

    implement express server setup with JSON parsing and URL encoding

--- src/server.js
+++ src/server.js
@@ -1,5 +1,15 @@
+import { app } from "./app.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 
 dotenv.config();
-connectDB();
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });

5a7cf59ca5ca79dddf36bde7f5e9debd040ef005
Hritik Chauhan <hritikchji@gmail.com>
2025-10-23 17:10:50 +0530

    add user model with password hashing and JWT token generation

--- src/config/db.js
+++ src/config/db.js
@@ -4,17 +4,16 @@ import { DB_NAME } from "../constants.js";
 let isConnected = false;
 
 const connectDB = async () => {
+  if (isConnected) return;
   try {
-    if (!isConnected) {
-      const connectionInstance = await mongoose.connect(
-        `${process.env.MONGODB_URL}/${DB_NAME}`
-      );
-      isConnected = true;
-      console.log(
-        "MongoDB connected successfully: ",
-        connectionInstance.connection.host
-      );
-    }
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URL}/${DB_NAME}`
+    );
+    isConnected = true;
+    console.log(
+      "MongoDB connected successfully: ",
+      connectionInstance.connection.host
+    );
   } catch (error) {
     console.error("MongoDB connection failed:", error);
     process.exit(1);
---
+++ src/models/user.model.js
@@ -0,0 +1,81 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    googleId: {
+      type: String,
+      unique: true,
+      sparse: true, // important
+    },
+    profileImage: {
+      type: String, // cloudinary url
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      name: this.name,
+      username: this.username,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+export const User = mongoose.model("User", userSchema);

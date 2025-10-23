d1d1a9c9154e590e941a8fe298d619e38ebdf438
Hritik Chauhan <hritikchji@gmail.com>
2025-10-23 17:55:36 +0530

    implement user register route

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 import express from "express";
+import userRoutes from "./routes/user.route.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+//routes
+app.use("/api/v1/auth", userRoutes);
 
 export { app };

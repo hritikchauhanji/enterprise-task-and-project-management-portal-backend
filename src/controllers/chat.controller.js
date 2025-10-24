6bb2ab70a66b4039a83ed8d3e4f256199e47df7c
Hritik Chauhan <hritikchji@gmail.com>
2025-10-25 01:42:27 +0530

    update chat api

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -1,13 +1,35 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { Chat } from "../models/chat.model.js";
+import { Project } from "../models/project.model.js";
+import { ApiError } from "../utils/apiError.js";
+import { UserRolesEnum } from "../constants.js";
 
 // get messages for a project
 const getProjectMessages = asyncHandler(async (req, res) => {
   const { projectId } = req.params;
 
+  const project = await Project.findById(projectId);
+
+  if (!project) {
+    throw new ApiError(400, "Project not found");
+  }
+
+  const canAccessProject = (project, user) => {
+    const isAdmin = user.role === UserRolesEnum.ADMIN;
+    const isCreator = project.createdBy.toString() === user._id.toString();
+    const isMember = project.members.some(
+      (memberId) => memberId.toString() === user._id.toString()
+    );
+
+    return isAdmin || isCreator || isMember;
+  };
+
+  if (!canAccessProject(project, req.user)) {
+    throw new ApiError(403, "You are not authorized to access this project");
+  }
   const messages = await Chat.find({ projectId }).sort({ createdAt: 1 });
 
-  return res.status(200).json(200, messages, "Message fetched Successfully");
+  res.status(200).json({ success: true, messages });
 });
 
 export { getProjectMessages };
--- src/routes/chat.routes.js
+++ src/routes/chat.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { getProjectMessages } from "../controllers/chat.controller.js";
 import { getProjectMessagesValidator } from "../validators/chat.validator.js";
-import { validate } from "../validators/validate";
+import { validate } from "../validators/validate.js";
 
 const router = Router();
 

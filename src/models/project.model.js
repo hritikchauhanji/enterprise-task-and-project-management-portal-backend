0af741c7266cbcb03764ab858452f370397a07ab
Hritik Chauhan <hritikchji@gmail.com>
2025-10-24 11:31:11 +0530

    add project model with schema definition for project management

---
+++ src/models/project.model.js
@@ -0,0 +1,31 @@
+import mongoose, { Schema } from "mongoose";
+
+const projectSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    deadline: {
+      type: Date,
+    },
+    members: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Project = mongoose.model("Project", projectSchema);

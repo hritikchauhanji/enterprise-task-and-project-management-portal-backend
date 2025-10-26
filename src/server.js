31613cceba1e48d142467005c10c809c83f4fa50
Hritik Chauhan <hritikchji@gmail.com>
2025-10-26 19:43:39 +0530

    update

--- src/server.js
+++ src/server.js
@@ -1,17 +1,14 @@
 import { app } from "./app.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
-import { initSocketServer } from "./socketServer.js";
 
 dotenv.config();
 
 connectDB()
   .then(() => {
-    const server = app.listen(process.env.PORT || 5000, () => {
+    app.listen(process.env.PORT || 5000, () => {
       console.log(`Server is running at port : ${process.env.PORT}`);
     });
-
-    // initSocketServer(server);
   })
   .catch((err) => {
     console.log("MONGO db connection failed !!!", err);
